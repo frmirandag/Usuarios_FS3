41c1bc4a10c8aec253ca94f4b8d281c4fe9e1bc6
frmirandag <fr.mirandag@duocuc.cl>
2024-11-18 23:11:10 -0300

    new commit 18-11-2024

--- tienda-online-usuarios/src/main/java/com/biblioteca/repository/UsuarioRepository.java
+++ tienda-online-usuarios/src/main/java/com/biblioteca/repository/UsuarioRepository.java
@@ -1,7 +1,6 @@
 package com.biblioteca.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.biblioteca.model.Usuario; // Cambiado de controller a model
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
